459f11ca35216e8d58a34d47a83b041473d9a047
x <>
2024-08-10 16:50:37 +0800

    userinfo

--- src/pages/user/index.jsx
+++ src/pages/user/index.jsx
@@ -3,13 +3,18 @@ import styles from "./index.module.scss"
 import Tabbar from '../../components/Tabbar';
 import Navbar from '../../components/Navbar';
 import Body from '../../components/Body';
+import { Avatar } from '@nutui/nutui-react-taro';
+import { ArrowSize6 } from '@nutui/icons-react-taro'
 
 export default () => {
     return (
         <View>
             <Navbar title="用户中心" ></Navbar>
             <Body>
-                用户中心
+                <View className={styles.userInfo}>
+                    <Avatar size="large" />
+                    <View className={styles.name}>登录/注册<ArrowSize6 width="var(--nutui-font-text)" /></View>
+                </View>
             </Body>
             <Tabbar value={4} />
         </View>
