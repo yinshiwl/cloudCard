5cc8a520d80aa1969c8bfc4ab7ac2f4e79bdc0cf
x <>
2024-08-13 17:18:31 +0800

    FixedNav

--- src/components/CardTheme/DefaultTheme/index.jsx
+++ src/components/CardTheme/DefaultTheme/index.jsx
@@ -1,7 +1,8 @@
 import { Text, View } from "@tarojs/components";
 import styles from "./index.module.scss";
-import { Avatar } from "@nutui/nutui-react-taro";
+import { Avatar, FixedNav } from "@nutui/nutui-react-taro";
 import GbIcons from "../../GbIcons";
+import { useState } from "react";
 
 export default () => {
     return (
@@ -20,19 +21,19 @@ export default () => {
                     公司
                 </View>
                 <View className={styles.moreInfo}>
-                    <Row icon="phone" title="手机号" extra="打电话" />
-                    <Row icon="" title="微信号" extra="加微信" />
-                    <Row icon="" title="邮箱" extra="复制" />
-                    <Row icon="" title="地址" extra="地图" />
+                    <Row icon="phone-fill" title="手机号" extra="打电话" />
+                    <Row icon="weixin" title="微信号" extra="加微信" />
+                    <Row icon="email-fill" title="邮箱" extra="复制" />
+                    <Row icon="location-fill" title="地址" extra="地图" />
                 </View>
             </View>
             <View className={styles.otherInfo}>
                 test222
             </View>
+            <BbFixedNav />
         </View>
     );
 }
-
 
 function Row({ icon, title, extra }) {
     return (
@@ -43,8 +44,52 @@ function Row({ icon, title, extra }) {
             </View>
             <View className={styles.right}>
                 <Text>{extra}</Text>
-                <GbIcons name="arrow" size="small" color="primary" />
+                <GbIcons name="arrow-right" size="small" color="primary" />
             </View>
         </View>
+    );
+}
+
+function BbFixedNav() {
+    const [visible, setVisible] = useState(true)
+    const list = [
+        {
+            id: 1,
+            text: '编辑资料',
+            icon: 'https://img12.360buyimg.com/imagetools/jfs/t1/119490/8/9568/1798/5ef83e95E968c69a6/dd029326f7d5042e.png',
+        },
+        {
+            id: 2,
+            text: '配置主题',
+            icon: 'https://img12.360buyimg.com/imagetools/jfs/t1/119490/8/9568/1798/5ef83e95E968c69a6/dd029326f7d5042e.png',
+        },
+        {
+            id: 3,
+            text: '预览名片',
+            icon: 'https://img12.360buyimg.com/imagetools/jfs/t1/119490/8/9568/1798/5ef83e95E968c69a6/dd029326f7d5042e.png',
+        },
+        {
+            id: 4,
+            text: '生成海报',
+            icon: 'https://img12.360buyimg.com/imagetools/jfs/t1/119490/8/9568/1798/5ef83e95E968c69a6/dd029326f7d5042e.png',
+        },
+    ]
+    const onChange = (value) => {
+        setVisible(value)
+    }
+    const onSelect = (item, event) => {
+        console.log(item, event)
+    }
+    return (
+        <FixedNav
+            type='left'
+            list={list}
+            inactiveText="展开"
+            activeText="收起"
+            visible={visible}
+            onChange={onChange}
+            onSelect={onSelect}
+            position={{ bottom: '52px' }}
+        />
     );
 }
