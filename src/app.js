1e0f5eb07b9629c4db24a96d21c3df52b4170a71
x <>
2024-09-04 11:31:01 +0800

    fix

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import { useDidShow, useDidHide } from '@tarojs/taro'
 // 全局样式
 import './app.scss'
 import '@nutui/nutui-react-taro/dist/styles/theme-jmapp.scss'
-import './assets/css/iconfont.css'
+import './assets/iconfont/iconfont.css'
 
 function App(props) {
   // 可以使用所有的 React Hooks
