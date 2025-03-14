dabb598693ad9aed33f631ff48054ce5545bef5a
xu <xu@example.com>
2025-03-14 21:28:39 +0800

    轮播图

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,18 +1,15 @@
 import Tabbar from '../../components/Tabbar'
 import Navbar from '../../components/Navbar'
 import Body from '../../components/Body'
-import CardList from '../../components/CardList'
 import Page from '../../components/Page'
-import useCardPage from '../../common/hooks/useCardPage'
-import Empty from './parts/Empty'
+import Swiper from './parts/Swiper'
 
 export default () => {
-  const { cardPage, getCardPage } = useCardPage();
   return (
     <Page>
       <Navbar title="云联名片" />
       <Body hasTabbar>
-        {cardPage?.data?.length === 0 ? <Empty /> : <CardList currentPage={cardPage} loadData={getCardPage} />}
+        <Swiper />
       </Body>
       <Tabbar value={0} />
     </Page>
---
+++ src/pages/index/parts/Swiper/index.jsx
@@ -0,0 +1,41 @@
+import { Swiper } from "@nutui/nutui-react-taro";
+import styles from "./index.module.scss";
+import { View } from "@tarojs/components";
+
+const Swiper = () => {
+    const list = [
+        'https://storage.360buyimg.com/jdc-article/NutUItaro34.jpg',
+        'https://storage.360buyimg.com/jdc-article/NutUItaro2.jpg',
+        'https://storage.360buyimg.com/jdc-article/welcomenutui.jpg',
+        'https://storage.360buyimg.com/jdc-article/fristfabu.jpg',
+    ]
+    const onChange = (e) => {
+        console.log(`onChange is trigger ${e}`)
+    }
+    return (
+        <View className={styles.swiper}>
+            <Swiper
+                defaultValue={1}
+                autoPlay
+                indicator
+                loop
+                circular
+                onChange={onChange}
+            >
+                {list.map((item, index) => (
+                    <Swiper.Item key={item}>
+                        <img
+                            width="100%"
+                            height="100%"
+                            onClick={() => console.log(index)}
+                            src={item}
+                            alt=""
+                        />
+                    </Swiper.Item>
+                ))}
+            </Swiper>
+        </View>
+    );
+}
+
+export default Swiper;
