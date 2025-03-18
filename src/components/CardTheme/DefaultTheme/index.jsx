fd2a84e6963d41a98ed25b1406743e818d3beecc
x <>
2025-03-18 19:09:00 +0800

    fix

--- src/components/CardTheme/DefaultTheme/index.jsx
+++ src/components/CardTheme/DefaultTheme/index.jsx
@@ -1,13 +1,11 @@
 import { Text, View } from "@tarojs/components";
 import styles from "./index.module.scss";
 import GbIcons from "../../GbIcons";
-import { useEffect, useState } from "react";
-import classNames from "classnames";
 import GbAvatar from "../../GbAvatar";
 import GbImagePreview from "../../GbImagePreview";
 import Taro from "@tarojs/taro";
 import utils from "../../../common/utils";
-import { Loading } from "@nutui/nutui-react-taro";
+import Operate from "./parts/Operate";
 
 export default ({ cardInfo }) => {
     const { name, position, company, description, phone, weChat, address, email, avatar, album } = cardInfo || {}
@@ -72,54 +70,6 @@ function Row({ icon, title, extra, onClick }) {
             <View className={styles.right}>
                 <Text className={styles.extra}>{extra}</Text>
                 <GbIcons name="arrow-right" size="small" color="primary" />
-            </View>
-        </View>
-    );
-}
-
-function Operate({ cardInfo }) {
-    const [loading, setLoading] = useState(false);
-    const [_cardInfo, setCardInfo] = useState(cardInfo);
-    const { liked = false, collected = false, likeCount = 0, collectCount = 0, browseCount = 0, id } = _cardInfo || {};
-    useEffect(() => {
-        setCardInfo(cardInfo)
-    }, [cardInfo])
-    return (
-        <View className={styles.operate}>
-            <View className={classNames(styles.operateItem, styles.disableClick)}>
-                <View className={styles.value}>
-                    <GbIcons name="eye" />
-                    <Text className={styles.valueText}>{browseCount}</Text>
-                </View>
-                <Text>浏览</Text>
-            </View>
-            <View className={classNames(styles.operateItem, { [styles.collect]: collected })} onClick={async () => {
-                if (loading) return
-                setLoading(true);
-                const resp = await utils.onCollect(id, !collected)
-                setLoading(false);
-                if (resp.status !== 0) return
-                setCardInfo({ ..._cardInfo, collected: !collected, collectCount: collectCount + (collected ? -1 : 1) })
-            }}>
-                <View className={styles.value}>
-                    <GbIcons name={collected ? "star-fill" : "star"} color={collected ? "var(--nutui-orange-6)" : null} />
-                    <Text className={styles.valueText}>{collectCount}</Text>
-                </View>
-                <Text>{loading ? <Loading type="circular" /> : '收藏'}</Text>
-            </View>
-            <View className={classNames(styles.operateItem, { [styles.like]: liked })} onClick={async () => {
-                if (loading) return
-                setLoading(true);
-                const resp = await utils.onLike(id, !liked)
-                setLoading(false);
-                if (resp.status !== 0) return
-                setCardInfo({ ..._cardInfo, liked: !liked, likeCount: likeCount + (liked ? -1 : 1) })
-            }}>
-                <View className={styles.value}>
-                    <GbIcons name={liked ? "love-fill" : "love"} color={liked ? "var(--nutui-red-8)" : null} />
-                    <Text className={styles.valueText}>{likeCount}</Text>
-                </View>
-                <Text>{loading ? <Loading type="circular" /> : '点赞'}</Text>
             </View>
         </View>
     );
---
+++ src/components/CardTheme/DefaultTheme/parts/Operate/index.jsx
@@ -0,0 +1,57 @@
+import { Text, View } from "@tarojs/components";
+import { useEffect, useState } from "react";
+import styles from "./index.module.scss";
+import classNames from "classnames";
+import GbIcons from "../../../../GbIcons";
+import utils from "../../../../../common/utils";
+import { Loading } from "@nutui/nutui-react-taro";
+
+const Operate = ({ cardInfo }) => {
+    const [loading, setLoading] = useState(false);
+    const [_cardInfo, setCardInfo] = useState(cardInfo);
+    const { liked = false, collected = false, likeCount = 0, collectCount = 0, browseCount = 0, id } = _cardInfo || {};
+    useEffect(() => {
+        setCardInfo(cardInfo)
+    }, [cardInfo])
+    return (
+        <View className={styles.operate}>
+            <View className={classNames(styles.operateItem, styles.disableClick)}>
+                <View className={styles.value}>
+                    <GbIcons name="eye" />
+                    <Text className={styles.valueText}>{browseCount}</Text>
+                </View>
+                <Text>浏览</Text>
+            </View>
+            <View className={classNames(styles.operateItem, { [styles.collect]: collected })} onClick={async () => {
+                if (loading) return
+                setLoading(true);
+                const resp = await utils.onCollect(id, !collected)
+                setLoading(false);
+                if (resp.status !== 0) return
+                setCardInfo({ ..._cardInfo, collected: !collected, collectCount: collectCount + (collected ? -1 : 1) })
+            }}>
+                <View className={styles.value}>
+                    <GbIcons name={collected ? "star-fill" : "star"} color={collected ? "var(--nutui-orange-6)" : null} />
+                    <Text className={styles.valueText}>{collectCount}</Text>
+                </View>
+                <Text>{loading ? <Loading type="circular" /> : '收藏'}</Text>
+            </View>
+            <View className={classNames(styles.operateItem, { [styles.like]: liked })} onClick={async () => {
+                if (loading) return
+                setLoading(true);
+                const resp = await utils.onLike(id, !liked)
+                setLoading(false);
+                if (resp.status !== 0) return
+                setCardInfo({ ..._cardInfo, liked: !liked, likeCount: likeCount + (liked ? -1 : 1) })
+            }}>
+                <View className={styles.value}>
+                    <GbIcons name={liked ? "love-fill" : "love"} color={liked ? "var(--nutui-red-8)" : null} />
+                    <Text className={styles.valueText}>{likeCount}</Text>
+                </View>
+                <Text>{loading ? <Loading type="circular" /> : '点赞'}</Text>
+            </View>
+        </View>
+    );
+}
+
+export default Operate;
