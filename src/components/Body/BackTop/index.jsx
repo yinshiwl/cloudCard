1023a2616db6ed01df904b47df12b1c257155ddd
x <>
2025-03-19 11:45:21 +0800

    fix

--- src/components/Body/BackTop/index.jsx
+++ src/components/Body/BackTop/index.jsx
@@ -1,15 +1,22 @@
 import { View } from "@tarojs/components";
 import styles from "./index.module.scss";
-import { DoubleArrowUp } from '@nutui/icons-react-taro'
+import { DoubleArrowUp } from "@nutui/icons-react-taro";
 
-const BackTop = ({ setBackTop }) => {
-    return (
-        <View className={styles.root} onClick={() => {
-            setBackTop(true)
-        }}>
-            <DoubleArrowUp className='nut-icon-am-jump nut-icon-am-infinite' />
-        </View>
-    );
-}
+const BackTop = ({ setBackTop, showBackTop }) => {
+	return (
+		<View
+			className={styles.root}
+			style={{
+				transition: "all 0.3s ease-in-out",
+				opacity: showBackTop ? 1 : 0,
+			}}
+			onClick={() => {
+				setBackTop(true);
+			}}
+		>
+			<DoubleArrowUp className="nut-icon-am-jump nut-icon-am-infinite" />
+		</View>
+	);
+};
 
 export default BackTop;
--- src/components/Body/index.jsx
+++ src/components/Body/index.jsx
@@ -1,27 +1,31 @@
 import { ScrollView } from "@tarojs/components";
 import utils from "../../common/utils";
-import styles from "./index.module.scss"
+import styles from "./index.module.scss";
 import { SafeArea } from "@nutui/nutui-react-taro";
 import { useState } from "react";
 import BackTop from "./BackTop";
 
 export default ({ children, hasTabbar }) => {
-    const { navBarHeight } = utils.getNavBarData();
-    const [backTop, setBackTop] = useState(false);
-    const [showBackTop, setShowBackTop] = useState(false);
-    return (
-        <ScrollView className={styles.root}
-            scrollY
-            scrollTop={backTop ? 0 : undefined}
-            onScroll={(e) => {
-                const { scrollTop } = e.target;
-                setShowBackTop(scrollTop > 500);
-                setBackTop(false);
-            }}
-            style={{ top: `${navBarHeight}px`, bottom: hasTabbar ? '52px' : '0' }}>
-            {children}
-            {showBackTop && <BackTop setBackTop={setBackTop} />}
-            <SafeArea position="bottom" />
-        </ScrollView>
-    );
-}
+	const { navBarHeight } = utils.getNavBarData();
+	const [backTop, setBackTop] = useState(false);
+	const [showBackTop, setShowBackTop] = useState(false);
+	return (
+		<ScrollView
+			className={styles.root}
+			scrollY
+			scrollTop={backTop ? 0 : undefined}
+			scrollWithAnimation
+			enableBackToTop
+			onScroll={(e) => {
+				const { scrollTop } = e.target;
+				setShowBackTop(scrollTop > 500);
+				setBackTop(false);
+			}}
+			style={{ top: `${navBarHeight}px`, bottom: hasTabbar ? "52px" : "0" }}
+		>
+			{children}
+			<BackTop setBackTop={setBackTop} showBackTop={showBackTop} />
+			<SafeArea position="bottom" />
+		</ScrollView>
+	);
+};
