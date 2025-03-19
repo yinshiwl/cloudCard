314701b1e8c639801c942bd588cfc45589f3cf3b
xu <xu@example.com>
2025-03-20 01:15:22 +0800

    fix

--- src/pages/channel/index.jsx
+++ src/pages/channel/index.jsx
@@ -1,24 +1,28 @@
-import { Text } from '@tarojs/components';
-import Page from '../../components/Page';
-import Tabbar from '../../components/Tabbar';
-import Body from '../../components/Body';
-import Navbar from '../../components/Navbar';
-import styles from './index.module.scss';
-import InfoCard from './parts/InfoCard';
-import TotalData from './parts/TotalData';
+import { Text, View } from "@tarojs/components";
+import Page from "../../components/Page";
+import Tabbar from "../../components/Tabbar";
+import Body from "../../components/Body";
+import Navbar from "../../components/Navbar";
+import styles from "./index.module.scss";
+import InfoCard from "./parts/InfoCard";
+import TotalData from "./parts/TotalData";
 
 const Channel = () => {
-    return (
-        <Page className={styles.root}>
-            <Navbar title="渠道中心" titleCenter fontSize="40rpx" ></Navbar>
-            <Body hasTabbar>
-                <InfoCard />
-                <Text className={styles.tips}>注：合作商可获得子渠道收益10%【官方奖励，不影响下级收益】</Text>
-                <TotalData />
-            </Body>
-            <Tabbar value={1} />
-        </Page>
-    );
-}
+	return (
+		<Page className={styles.root}>
+			<Navbar title="渠道中心" titleCenter fontSize="40rpx"></Navbar>
+			<Body hasTabbar>
+				<View className={styles.content}>
+					<InfoCard />
+					<Text className={styles.tips}>
+						注：合作商可获得子渠道收益10%【官方奖励，不影响下级收益】
+					</Text>
+					<TotalData />
+				</View>
+			</Body>
+			<Tabbar value={1} />
+		</Page>
+	);
+};
 
 export default Channel;
