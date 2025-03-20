d66d6da79477224a5da1a642b0bea27d1764fa53
x <>
2025-03-20 23:17:57 +0800

    fix

--- src/pages/gameFilter/index.jsx
+++ src/pages/gameFilter/index.jsx
@@ -1,8 +1,12 @@
-import { Button, Menu } from "@nutui/nutui-react-taro";
+import { Button, Menu, SearchBar } from "@nutui/nutui-react-taro";
 import { useRef, useState } from "react";
 import Page from "../../components/Page";
 import Navbar from "../../components/Navbar";
 import Body from "../../components/Body";
+import GameList from "../../components/GameList";
+import Card from "../../components/Card";
+import styles from "./index.module.scss";
+import { View } from "@tarojs/components";
 
 export default () => {
 	const [options] = useState([
@@ -15,32 +19,38 @@ export default () => {
 		<Page>
 			<Navbar title="游戏列表" back></Navbar>
 			<Body full>
-				<Menu
-					onClose={(a, f) => {
-						console.log(a, f);
-					}}
-				>
-					<Menu.Item options={options} defaultValue={0} />
-					<Menu.Item title="筛选" ref={itemRef}>
-						<div
-							style={{
-								width: "50%",
-								lineHeight: "28px",
-								padding: "0 30px",
-							}}
-						>
-							自定义内容
-						</div>
-						<Button
-							size="small"
-							onClick={() => {
-								itemRef.current?.toggle(false);
-							}}
-						>
-							确认
-						</Button>
-					</Menu.Item>
-				</Menu>
+				<View className={styles.head}>
+					<SearchBar shape="round" maxLength={5} />
+					<Menu
+						onClose={(a, f) => {
+							console.log(a, f);
+						}}
+					>
+						<Menu.Item options={options} defaultValue={0} />
+						<Menu.Item title="筛选" ref={itemRef}>
+							<div
+								style={{
+									width: "50%",
+									lineHeight: "28px",
+									padding: "0 30px",
+								}}
+							>
+								自定义内容
+							</div>
+							<Button
+								size="small"
+								onClick={() => {
+									itemRef.current?.toggle(false);
+								}}
+							>
+								确认
+							</Button>
+						</Menu.Item>
+					</Menu>
+				</View>
+				<Card>
+					<GameList />
+				</Card>
 			</Body>
 		</Page>
 	);
--- src/pages/index/parts/NoticeBar/index.jsx
+++ src/pages/index/parts/NoticeBar/index.jsx
@@ -1,34 +1,36 @@
-import { NoticeBar as NutuiNoticeBar } from '@nutui/nutui-react-taro'
-import { ArrowSize6 } from '@nutui/icons-react-taro'
-import { Text, View } from '@tarojs/components';
+import { NoticeBar as NutuiNoticeBar } from "@nutui/nutui-react-taro";
+import { ArrowSize6 } from "@nutui/icons-react-taro";
+import { View } from "@tarojs/components";
 import styles from "./index.module.scss";
-import Taro from '@tarojs/taro';
+import Taro from "@tarojs/taro";
 
 const NoticeBar = () => {
-    const horseLamp1 = [
-        'NoticeBar 公告栏AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
-        'Cascader 级联选择BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB',
-        'DatePicker 日期选择器EDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD',
-        'CheckBox 复选按钮',
-    ]
-    const onClick = () => {
-        Taro.navigateTo({ url: '/pages/notify/index' })
-    }
-    return (
-        <View className={styles.root}>
-            <NutuiNoticeBar
-                direction="vertical"
-                list={horseLamp1.map((item, index) => (
-                    <Text key={index} className={styles.text}>{item}</Text>
-                ))}
-                speed={10}
-                duration={1000}
-                height={30}
-                rightIcon={<ArrowSize6 onClick={onClick} />}
-                onClick={onClick}
-            />
-        </View>
-    );
-}
+	const horseLamp1 = [
+		"NoticeBar 公告栏AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+		"Cascader 级联选择BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
+		"DatePicker 日期选择器EDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD",
+		"CheckBox 复选按钮",
+	];
+	const onClick = () => {
+		Taro.navigateTo({ url: "/pages/notify/index" });
+	};
+	return (
+		<View className={styles.root}>
+			<NutuiNoticeBar
+				direction="vertical"
+				list={horseLamp1.map((item, index) => (
+					<View key={index} className={styles.text}>
+						{item}
+					</View>
+				))}
+				speed={10}
+				duration={2000}
+				height={30}
+				rightIcon={<ArrowSize6 onClick={onClick} />}
+				onClick={onClick}
+			/>
+		</View>
+	);
+};
 
 export default NoticeBar;
